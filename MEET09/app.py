880d3e04c407b0a34a77a3f85fb756ec45c772db
jimmyalfresco <152016053+jimmyalfresco@users.noreply.github.com>
2024-08-10 12:25:24 +0700

    Add files via upload

---
+++ MEET09/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/igs')
+def igs():
+    return render_template('igs.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
